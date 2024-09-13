f5878396912d92afa21b59fd49389fc2141bc4ae
OrregoMatias <orregomatias50@gmail.com>
2024-09-13 15:43:18 -0300

    mejores los controllers del backend

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -40,41 +40,68 @@ export const createTodoCtrl = (req, res) => {
 }
 
 export const updateTodoCtrl = (req, res) => {
-  const id = req.params.id;
+  const userId = req.user.id;
+  const { id } = req.params;
   const { title, completed } = req.body;
 
-  if (!title && completed === undefined) {
-    return res.status(400).json({ message: "Se requiere al menos un campo a actualizar" });
-  } else if (title && title === "") {
-    return res.status(400).json({ message: "El título no puede ser vacío" });
-  } else if (title && typeof title !== "string") {
-    return res.status(400).json({ message: "El título debe ser un texto" });
-  } else if (completed !== undefined && typeof completed !== "boolean") {
-    return res.status(400).json({ message: "El estado de la tarea debe ser un booleano" });
+  if (!title) {
+    return res.status(400).json({ message: "El título es requerido" });
+  } else if (typeof title !== "string") {
+    return res.status(400).json({ message: "El título debe ser un string" });
+  } else if (title.length < 3) {
+    return res
+      .status(400)
+      .json({ message: "El título debe tener al menos 3 caracteres" });
   }
 
-  const todo = database.todos.find((todo) => todo.id === id);
+  if (completed === undefined) {
+    return res
+      .status(400)
+      .json({ message: "El estado de la tarea es requerido" });
+  } else if (typeof completed !== "boolean") {
+    return res
+      .status(400)
+      .json({ message: "El estado de la tarea debe ser un booleano" });
+  }
 
-  if (!todo) {
+  const todoIndex = database.todos.findIndex((todo) => todo.id === Number(id));
+
+  if (todoIndex === -1) {
     return res.status(404).json({ message: "Tarea no encontrada" });
   }
 
-  todo.title = title;
-  todo.completed = completed;
+  if (database.todos[todoIndex].owner !== userId) {
+    return res
+      .status(403)
+      .json({ message: "No tienes permisos para modificar esta tarea" });
+  }
+
+  database.todos[todoIndex] = {
+    ...database.todos[todoIndex],
+    title,
+    completed,
+  };
 
   res.json({ message: "Tarea actualizada exitosamente" });
-}
+};
 
 export const deleteTodoCtrl = (req, res) => {
-  const id = req.params.id;
+  const userId = req.user.id;
+  const { id } = req.params;
 
-  const index = database.todos.findIndex((todo) => todo.id === id);
+  const todoIndex = database.todos.findIndex((todo) => todo.id === Number(id));
 
-  if (index === -1) {
+  if (todoIndex === -1) {
     return res.status(404).json({ message: "Tarea no encontrada" });
   }
 
-  database.todos.splice(index, 1);
+  if (database.todos[todoIndex].owner !== userId) {
+    return res
+      .status(403)
+      .json({ message: "No tienes permisos para eliminar esta tarea" });
+  }
+
+  database.todos.splice(todoIndex, 1);
 
   res.json({ message: "Tarea eliminada exitosamente" });
-}
+};
